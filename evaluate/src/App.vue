f6454b34348230b5bf1bdd3cd5c5ba81766c4359
zjh <zoujunhui@bilibili.co>
2021-06-07 16:49:19 +0800

    init

--- evaluate/src/App.vue
+++ evaluate/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- 使用 router-link 组件来导航. -->
+    <!-- 通过传入 `to` 属性指定链接. -->
+    <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+    <router-link to="/product-list">product</router-link>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
     <router-view/>
   </div>
 </template>
@@ -12,12 +17,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ evaluate/src/components/ProductList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>list</h1>
+    <p>{{msg}}</p>
+    <el-button @click="visible = true">Button</el-button>
+    <el-dialog :visible.sync="visible" title="Hello world">
+      <p>Try Element</p>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+  data () {
+    return {
+      msg: "我是 list 组件"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
