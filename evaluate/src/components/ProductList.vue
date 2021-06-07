989f2617362f3544f741df6e47b558d095e32c8c
zjh <zoujunhui@bilibili.co>
2021-06-07 17:11:50 +0800

    fix

--- evaluate/src/components/ProductList.vue
+++ evaluate/src/components/ProductList.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    <h1>list</h1>
-    <p>{{msg}}</p>
-    <el-button @click="visible = true">Button</el-button>
-    <el-dialog :visible.sync="visible" title="Hello world">
-      <p>Try Element</p>
-    </el-dialog>
+  <div id="app">
+    <template>
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="date" label="日期" width="180"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+      </el-table>
+    </template>
   </div>
 </template>
 
@@ -13,10 +14,26 @@
 export default {
   name: "ProductList",
   data () {
-    return {
-      msg: "我是 list 组件"
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
     }
-  }
 }
 </script>
 
