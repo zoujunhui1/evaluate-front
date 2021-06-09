be9779004175c9e8e509e3581d361492c5a3450e
zjh <zoujunhui@bilibili.co>
2021-06-10 00:44:33 +0800

    用户名+密码

--- evaluate/src/components/Login.vue
+++ evaluate/src/components/Login.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="login_container">
     <div class="login_box">
+      <!--头像区域-->
       <div class="avatar_box">
         <img src="../assets/logo.png" alt="">
       </div>
+      <!--登陆表单区域-->
+      <el-form ref="form"  label-width="0px" class="login_form">
+        <!--用户名-->
+        <el-form-item>
+          <el-input placeholder="用户名"></el-input>
+        </el-form-item>
+        <!--密码-->
+        <el-form-item >
+          <el-input placeholder="密码"></el-input>
+        </el-form-item>
+        <!--按钮-->
+        <el-form-item class="btns">
+          <el-button type="primary">提交</el-button>
+        </el-form-item>
+      </el-form >
     </div>
   </div>
 </template>
@@ -45,5 +61,16 @@ export default {
       border-radius: 50%;
       background-color: #eee;
     }
+  }
+  .btns{
+    display: flex;
+    justify-content: flex-end;
+  }
+  .login_form {
+    position: absolute;
+    bottom:0;
+    width :100%;
+    padding: 0 20px;
+    box-sizing: border-box;
   }
 </style>
