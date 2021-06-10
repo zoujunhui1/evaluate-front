e5d597c925e71de827a3d06cf97519b01087adb7
zjh <zoujunhui@bilibili.co>
2021-06-10 20:28:44 +0800

    退出

--- evaluate/src/components/Home.vue
+++ evaluate/src/components/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     Home组件
+    <el-button type="info" v-on:click="logout">退出</el-button>
   </div>
 </template>
 
 <script>
 export default {
-name: "Home"
+  name: "Home",
+  methods:{
+    logout(){
+      sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
