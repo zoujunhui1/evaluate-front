a8001294971c598ed7bdd6de56c0604dcf1bdd1b
zjh <zoujunhui@bilibili.co>
2021-06-11 19:09:28 +0800

    产品编辑

--- evaluate/src/components/GoodsList.vue
+++ evaluate/src/components/GoodsList.vue
@@ -27,7 +27,7 @@
         <el-table-column prop="" label="操作">
           <template slot-scope="scope">
             <el-tooltip effect="dark" content="编辑" placement="top-start" :enterable ="false">
-              <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+              <el-button type="primary" icon="el-icon-edit" size="mini" v-on:click="showEditDialog(scope.row.id)">编辑</el-button>
             </el-tooltip>
             <el-tooltip effect="dark" content="删除" placement="top-start" :enterable ="false">
               <el-button type="danger" icon="el-icon-delete"size="mini">删除</el-button>
@@ -55,9 +55,23 @@
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-    <el-button v-on:click="addDialogVisible = false">取 消</el-button>
-    <el-button type="primary" v-on:click="addGood">确 定</el-button>
-  </span>
+        <el-button v-on:click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" v-on:click="addGood">确 定</el-button>
+      </span>
+    </el-dialog>
+<!--商品编辑-->
+    <el-dialog title="商品编辑" :visible.sync="showDialogVisible"
+               width="50%" @close = "editDialogClosed">
+      <el-form :model="editForm" :rules="editFormRules"
+               ref="editFormRef" label-width="80px">
+        <el-form-item label="产品名称" prop="name">
+          <el-input v-model="editForm.name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="showDialogVisible = false">取 消</el-button>
+        <el-button type="primary" v-on:click="editGood">确 定</el-button>
+      </span>
     </el-dialog>
   </div>
 </template>
@@ -74,18 +88,27 @@ export default {
       },
       goodsList :[],
       total:0,
-      addDialogVisible:false,
+      addDialogVisible:false,//商品添加对话框
+      showDialogVisible:false,//商品编辑对话框
       //添加商品
       addForm:{
-        name :''
       },
-      //商品验证规则
+      //商品添加验证规则
       addFormRules:{
         name: [
           { required: true, message: '请输入产品名称', trigger: 'blur' },
           { min: 1, max: 15, message: '长度在 1 到 15 个字符', trigger: 'blur' }
         ],
-      }
+      },
+      editForm:{
+      },
+    //商品编辑验证规则
+      editFormRules:{
+        name: [
+          { required: true, message: '请输入产品名称', trigger: 'blur' },
+          { min: 1, max: 15, message: '长度在 1 到 15 个字符', trigger: 'blur' }
+        ],
+      },
     }
   },
   created () {
@@ -116,11 +139,41 @@ export default {
       this.$refs.addFormRef.validate(async valid=>{
         if (!valid) return
         const {data:res} = await this.$http.post('/evaluate/add',this.addForm)
-        console.log(res)
+        if (res.status > 0 ) return this.$message.error("添加失败")
         this.$message.success('添加成功');
         //隐藏对话框
         this.addDialogVisible = false
       })
+    },
+    //展示产品编辑框
+    async showEditDialog(id){
+      this.showDialogVisible = true
+      const {data:res} = await this.$http.get('/evaluate/list',{params: {'id':id}})
+      if (res.status > 0 || res.data.list.length === 0) return this.$message.error("获取产品失败")
+      this.editForm = res.data.list[0]
+    },
+    //编辑商品
+    editGood () {
+      this.$refs.editFormRef.validate(async valid=>{
+        if (!valid) return
+        const updateData = {
+          'id':this.editForm.id,
+          'name':this.editForm.name
+        }
+        const {data:res} = await this.$http.post('/evaluate/edit',updateData)
+        if (res.status > 0 ) return this.$message.error("编辑失败")
+        //隐藏对话框
+        this.showDialogVisible = false
+        //重新获取列表
+        await this.getGoodsList()
+        //提示成功
+        this.$message.success('编辑成功');
+
+      })
+    },
+    //监听修改编辑框关闭
+    editDialogClosed() {
+     this.$refs.editFormRef.resetFields()
     }
 
   }
