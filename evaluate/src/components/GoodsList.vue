afb9cd8b13cdc3f4de22d6f7225a8664605527fb
zjh <zoujunhui@bilibili.co>
2021-06-11 15:04:38 +0800

    分页

--- evaluate/src/components/GoodsList.vue
+++ evaluate/src/components/GoodsList.vue
@@ -24,8 +24,27 @@
       <el-table :data="goodsList" border stripe>
         <el-table-column prop="product_id" label="产品编号"></el-table-column>
         <el-table-column prop="name" label="名称" ></el-table-column>
-        <el-table-column prop="" label="操作" ></el-table-column>
+        <el-table-column prop="" label="操作">
+          <template slot-scope="scope">
+            <el-tooltip effect="dark" content="编辑" placement="top-start" :enterable ="false">
+              <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+            </el-tooltip>
+            <el-tooltip effect="dark" content="删除" placement="top-start" :enterable ="false">
+              <el-button type="danger" icon="el-icon-delete"size="mini">删除</el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
       </el-table>
+<!--分页-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.page"
+        :page-sizes="[10, 20, 35, 40]"
+        :page-size="queryInfo.count"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -47,12 +66,24 @@ export default {
     this.getGoodsList()
   },
   methods: {
+    //商品列表
    async getGoodsList(){
      const {data:res} = await this.$http.get('/evaluate/list',{params: this.queryInfo})
      if (res.status > 0 ) return this.$message.error("获取产品列表失败")
      this.goodsList = res.data.list
      this.total = res.data.total
-     console.log(this.total,this.goodsList )
+     this.queryInfo.count = res.data.count
+     this.queryInfo.page = res.data.page
+    },
+    //监听每页展示条数
+    handleSizeChange(newSize) {
+      this.queryInfo.count = newSize
+      this.getGoodsList()
+    },
+    //监听页码值改变的事件
+    handleCurrentChange (newPage) {
+      this.queryInfo.page =  newPage
+      this.getGoodsList()
     }
   }
 }
@@ -67,5 +98,8 @@ export default {
 .el-table {
   margin-top: 15px;
   font-size: 12px;
+}
+.el-pagination {
+  margin-top: 15px;
 }
 </style>
